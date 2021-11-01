09a66482ac9c6f9be280ebcf3b03c1194a021dec
eunseo2 <20180972@sungshin.ac.kr>
2021-11-02 00:23:03 +0900

    [#14] 음식 등록 시 storeId 설정

--- src/main/java/org/prgrms/kyu/controller/StoreController.java
+++ src/main/java/org/prgrms/kyu/controller/StoreController.java
@@ -1,23 +1,32 @@
 package org.prgrms.kyu.controller;
 
-import javax.naming.AuthenticationException;
+
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.prgrms.kyu.dto.JoinRequest;
-import org.prgrms.kyu.dto.StoreCreateRequest;
+
 import org.prgrms.kyu.service.StoreService;
+import org.prgrms.kyu.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 @RequiredArgsConstructor
 public class StoreController {
-  private final StoreService storeService;
+    private final StoreService storeService;
+    private final UserService userService;
 
-  @PostMapping("/store")
-  public String signUp(@ModelAttribute("storeCreateForm") StoreCreateRequest storeCreateRequest)
-      throws AuthenticationException {
-    storeService.save(storeCreateRequest);
-    return "redirect:/";
-  }
+
+    @GetMapping("/stores/{storeId}")
+    public String findById(@PathVariable("storeId") Long storeId, Model model,  Authentication authentication) throws NotFoundException {
+        model.addAttribute("userInfo",
+                userService.getUser(((UserDetails) authentication.getPrincipal()).getUsername()));
+        model.addAttribute("store", storeService.findById(storeId));
+        return "store/detail-view";
+    }
 }
