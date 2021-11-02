2eeb91b5a3f13e7e17af131ea0981fce16d7e393
eunseo2 <20180972@sungshin.ac.kr>
2021-11-02 15:14:44 +0900

    [feat: #14]  단일 메뉴 등록 화면 구현

--- src/main/java/org/prgrms/kyu/controller/StoreController.java
+++ src/main/java/org/prgrms/kyu/controller/StoreController.java
@@ -14,21 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-import lombok.RequiredArgsConstructor;
+
 import org.prgrms.kyu.dto.StoreCreateRequest;
 import org.prgrms.kyu.dto.UserInfo;
 import org.prgrms.kyu.entity.UserType;
 
 import org.prgrms.kyu.service.SecurityService;
-import org.prgrms.kyu.service.StoreService;
-import org.prgrms.kyu.service.UserService;
-import org.springframework.security.core.Authentication;
 
-import org.springframework.security.core.userdetails.UserDetails;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -45,8 +37,10 @@ public class StoreController {
 
     @GetMapping("/stores/{storeId}")
     public String findById(@PathVariable("storeId") Long storeId, Model model,  Authentication authentication) throws NotFoundException {
-        model.addAttribute("userInfo",
-                userService.getUser(((UserDetails) authentication.getPrincipal()).getUsername()));
+        if (securityService.isAuthenticated()) {
+            model.addAttribute("userInfo",
+                    userService.getUser(((UserDetails) authentication.getPrincipal()).getUsername()));
+        }
         model.addAttribute("store", storeService.findById(storeId));
         return "store/detail-view";
     }
